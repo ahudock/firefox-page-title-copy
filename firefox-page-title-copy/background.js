ffd35f6d57a72417673e674f7e51cff1e225b4c1
Andy Hudock <ahudock@pm.me>
2024-01-27 00:00:39 -0800

    refactor: rename ids and add console logging

--- firefox-page-title-copy/background.js
+++ firefox-page-title-copy/background.js
@@ -1,26 +1,34 @@
 browser.runtime.onInstalled.addListener(function () {
+    console.log("Page Title Copy: Creating menu items...");
+
     // Create context menu items
     browser.contextMenus.create({
-        id: "pageCopyTitle",
-        title: "Copy Page Title",
-        contexts: ["page"]
-    });
+            id: "copy-page-title-page",
+            title: "Copy Page Title",
+            contexts: ["page"]
+        }, function () {
+            console.log("Created page context menu item.");
+        }
+    );
 
     browser.contextMenus.create({
-        id: "tabCopyTitle",
-        title: "Copy Page Title",
-        contexts: ["tab"]
-    });
+            id: "copy-page-title-tab",
+            title: "Copy Page Title",
+            contexts: ["tab"]
+        }, function () {
+            console.log("Created tab context menu item.");
+        }
+    );
 });
 
 browser.contextMenus.onClicked.addListener(function (info, tab) {
-    if (info.menuItemId === "pageCopyTitle" || info.menuItemId === "tabCopyTitle") {
+    if (info.menuItemId === "copy-page-title-page" || info.menuItemId === "copy-page-title-tab") {
         browser.tabs.sendMessage(tab.id, {action: "copyPageTitle"});
     }
 });
 
 browser.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     if (request.action === "pageTitleCopied") {
-        console.log("Page title copied to clipboard");
+        console.log("Page title copied to clipboard.");
     }
 });
