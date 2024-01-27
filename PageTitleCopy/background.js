63ad8bfc3375ed0a1b931cdf39551c321df0f740
Andy Hudock <ahudock@pm.me>
2024-01-26 22:34:58 -0800

    fix: switch to browser object

--- PageTitleCopy/background.js
+++ PageTitleCopy/background.js
@@ -1,25 +1,25 @@
-chrome.runtime.onInstalled.addListener(function () {
+browser.runtime.onInstalled.addListener(function () {
     // Create context menu items
-    chrome.contextMenus.create({
+    browser.contextMenus.create({
         id: "pageCopyTitle",
         title: "Copy Page Title",
         contexts: ["page"]
     });
 
-    chrome.contextMenus.create({
+    browser.contextMenus.create({
         id: "tabCopyTitle",
         title: "Copy Page Title",
         contexts: ["tab"]
     });
 });
 
-chrome.contextMenus.onClicked.addListener(function (info, tab) {
+browser.contextMenus.onClicked.addListener(function (info, tab) {
     if (info.menuItemId === "pageCopyTitle" || info.menuItemId === "tabCopyTitle") {
-        chrome.tabs.sendMessage(tab.id, {action: "copyPageTitle"});
+        browser.tabs.sendMessage(tab.id, {action: "copyPageTitle"});
     }
 });
 
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+browser.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     if (request.action === "pageTitleCopied") {
         console.log("Page title copied to clipboard");
     }
